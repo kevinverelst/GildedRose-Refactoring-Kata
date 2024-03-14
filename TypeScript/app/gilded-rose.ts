65a5993c54e10e9471b62519594e19ce10c0809b
Kevin Verelst <kevin.verelst@codeer.be>
2024-03-14 23:54:58 +0100

    Technical test

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,13 +3,12 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name, sellIn, quality) {
+  constructor(name: string, sellIn: number, quality: number) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
-
 export class GildedRose {
   items: Array<Item>;
 
@@ -17,53 +16,49 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+  public updateQuality() {
+    this.items.forEach(item => {
+      switch (item.name) {
+        case 'Aged Brie':
+          this.updateAgedBrie(item);
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.updateBackstagePass(item);
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          // No changes for "Sulfuras, Hand of Ragnaros" as it never alters
+          break;
+        default:
+          this.updateOtherItems(item);
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
+    });
 
     return this.items;
+  }
+
+  private updateAgedBrie(item: Item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+      if (item.sellIn < 1 && item.quality < 50) item.quality += 1;
+    }
+    item.sellIn -= 1;
+  }
+
+  private updateBackstagePass(item: Item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+      if (item.sellIn < 11 && item.quality < 50) item.quality += 1;
+      if (item.sellIn < 6 && item.quality < 50) item.quality += 1;
+    }
+    if (item.sellIn < 1) item.quality = 0;
+    item.sellIn -= 1;
+  }
+
+  private updateOtherItems(item: Item) {
+    if (item.quality > 0) {
+      item.quality -= 1;
+      if (item.sellIn < 1 && item.quality > 0) item.quality -= 1;
+    }
+    item.sellIn -= 1;
   }
 }
--- TypeScript/test/jest/approvals.spec.ts
+++ TypeScript/test/jest/approvals.spec.ts
@@ -2,7 +2,7 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 /**
  * This unit test uses [Jest Snapshot](https://goo.gl/fbAQLP).
- * 
+ *
  * There are two test cases here with different styles:
  * <li>"foo" is more similar to the unit test from the 'Java' version
  * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
@@ -37,17 +37,48 @@ describe('Gilded Rose Approval', () => {
     process.argv = originalProcessArgv;
   });
 
+  it('should construct with empty items array if no items provided', () => {
+    const gildedRose = new GildedRose();
+    expect(gildedRose.items).toEqual([]);
+  });
+
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-  
+
     expect(items).toMatchSnapshot();
+  });
+
+  it('should decrease quality and sellIn for regular item', () => {
+    const gildedRose = new GildedRose([new Item('Normal item', 3, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+    expect(items[0].sellIn).toBe(2);
+  });
+
+  it('should increase quality for Aged Brie', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+
+  it('should handle Backstage passes correctly', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(13);
+  });
+
+  it('should not decrease quality or sellIn for Sulfuras, Hand of Ragnaros', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+    expect(items[0].sellIn).toBe(5);
   });
 
   it('should thirtyDays', () => {
     process.argv = ["<node>", "<script", "30"];
     require('../golden-master-text-test.ts');
-       
+
     expect(gameConsoleOutput).toMatchSnapshot();
   });
 
